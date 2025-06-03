09efb0478d674d4ce4c9d0afa4d21ee6594bb955
SFGroupOfCompaniesLtd <it@sfgroup.co.tz>
2025-06-04 00:07:00 +0300

    Add inventory_stock_movement/models/__init__.py 1748984819750

---
+++ inventory_stock_movement/models/__init__.py
@@ -0,0 +1,6 @@
+from . import stock_movement
+from . import stock_movement_line
+from . import stock_movement_reason
+from . import stock_location
+from . import product
+from . import res_config_settings
